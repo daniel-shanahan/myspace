337d17b1b59fbbd252618db1f81a9532bbdee659
Daniel Shanahan <daniel.patrick.shanahan@gmail.com>
2023-09-10 15:50:56 -0400

    Updated individual blog page with statically generated content

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -6,6 +6,16 @@ interface Post {
   slug: string;
 }
 
+export async function generateStaticParams() {
+  const posts: Post[] = await fetch("http://localhost:3000/api/content").then(
+    (res) => res.json()
+  );
+
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
+}
+
 interface Props {
   params: {
     slug: string;
